5d67b61317020b9ed961dd8d7a8b108c0f8f279d
Viktor Shmatko <37986452+Viktor727@users.noreply.github.com>
2025-02-03 20:50:38 +0200

    Homework 4 is done related to JavaScript primitive data types

---
+++ jsBasics/task3.js
@@ -0,0 +1,15 @@
+// Завдання 3: Числа та булі
+
+// Створіть змінну яка буде символізувати вік певної персони.
+// Після чого створіть булеву змінну яка буде визначати чи особа повнолітня базуючись на заченні з попередньої змінної.
+// Виведіть усі змінні в консоль
+
+const user = { 
+  name: "John", 
+  surname: "Trump", 
+  age: 18
+}
+
+const EnoughtOldPerson = user.age >= 18;
+
+console.log(`Is ${user.name} ${user.surname} full age? `, EnoughtOldPerson ? "Yes, he is" : "No, he isn't");
