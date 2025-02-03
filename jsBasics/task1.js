9667c96039fb1199d6c8b631f8fe7990ce0328a1
Viktor Shmatko <37986452+Viktor727@users.noreply.github.com>
2025-02-03 20:50:38 +0200

    Homework 4 is done related to JavaScript primitive data types

---
+++ jsBasics/task1.js
@@ -0,0 +1,16 @@
+// Завдання 1: Оголошення змінних для примітивних типів
+
+// Створіть програму, в якій ви оголосите змінні для кожного з примітивних типів даних: 
+// рядок, число, буль, null та undefined. Присвойте їм значення та виведіть кожну змінну на консоль.
+
+const string = 'Hello world!';
+const number = 69;
+const boolean = true;
+const somethingOne = null;
+const somethingTwo = undefined;
+
+console.log("string is: ", string);
+console.log("number is: ", number);
+console.log("boolean is: ", boolean);
+console.log("null is: ", somethingOne);
+console.log("undefined is: ", somethingTwo);
